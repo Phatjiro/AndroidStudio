a3ffcfbe92709b6861e75e58cde41b3576e51b8a
Phatjiro <nguyenvanphat911@gmail.com>
2022-04-19 17:22:59 +0700

    up tuan 9, co ban

---
+++ AndroidProject/Tuan9/app/src/main/java/com/mobile/tuan9/MainActivity.java
@@ -0,0 +1,33 @@
+package com.mobile.tuan9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLayoutSignIn();
+            }
+        });
+    }
+
+    public void openLayoutSignIn() {
+        Intent intent = new Intent(this, SignIn.class);
+        startActivity(intent);
+    }
+}
